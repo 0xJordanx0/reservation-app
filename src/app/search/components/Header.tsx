ad4c0794d8e84c0944ed7ad48cf45ef1ed428ed1
0xJordanx0 <ahmedjamlan12@gmail.com>
2023-09-04 23:51:59 +0400

    Refactored search into component

--- src/app/search/components/Header.tsx
+++ src/app/search/components/Header.tsx
@@ -1,20 +1,12 @@
 import React from "react";
 import NavLinks from "../../components/NavLinks";
+import SearchBar from "@/app/components/SearchBar";
 
 export default function Header() {
   return (
     <div className="flex justify-center bg-[#FB8500] p-4 gap-2 items-center">
       <NavLinks />
-      <div className="flex gap-2 justify-center w-1/2">
-        <input
-          name="search"
-          className="rounded p-2.5 w-1/2"
-          placeholder="Search Resturants"
-        />
-        <button className="bg-[#FFB703] hover:bg-yellow-500 hover:text-white p-2.5 rounded">
-          Search
-        </button>
-      </div>
+      <SearchBar />
     </div>
   );
 }
