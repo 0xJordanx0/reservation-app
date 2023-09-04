5eaedad569f15ce5818e5248ad6fab59c1d54f72
0xJordanx0 <ahmedjamlan12@gmail.com>
2023-09-04 23:51:39 +0400

    Refactored into server component and moved search

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,12 +1,7 @@
-"use client";
-
 import Navbar from "./Navbar";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
+import SearchBar from "./SearchBar";
 
 export default function Header() {
-  const router = useRouter();
-  const [location, setLocation] = useState("");
   return (
     <section>
       <div className="hero bg-[#FB8500] h-[60vh]">
@@ -18,23 +13,7 @@ export default function Header() {
               <h2 className="text-xl font-semibold">Online Foods Booking</h2>
             </div>
           </div>
-          <div className="flex gap-2 sm:w-2/3 md:w-1/2 xl:w-1/3">
-            <input
-              name="search"
-              value={location}
-              onChange={(e) => setLocation(e.target.value)}
-              className="rounded p-2.5 w-full"
-              placeholder="Search Resturants"
-            />
-            <button
-              className="bg-[#FFB703] hover:bg-yellow-500 hover:text-white p-2.5 rounded"
-              onClick={() => {
-                if (location === "alfonso") return router.push("/search");
-              }}
-            >
-              Search
-            </button>
-          </div>
+         <SearchBar />
         </div>
       </div>
     </section>
---
+++ src/app/components/SearchBar.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import React from "react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+export default function SearchBar() {
+  const router = useRouter();
+  const [location, setLocation] = useState("");
+  return (
+    <div className="flex gap-2 sm:w-2/3 md:w-1/2 xl:w-1/3">
+      <input
+        name="search"
+        value={location}
+        onChange={(e) => setLocation(e.target.value)}
+        className="rounded p-2.5 w-full"
+        placeholder="Search Resturants"
+      />
+      <button
+        className="bg-[#FFB703] hover:bg-yellow-500 hover:text-white p-2.5 rounded"
+        onClick={() => {
+          if (location === "alfonso") return router.push("/search");
+        }}
+      >
+        Search
+      </button>
+    </div>
+  );
+}
