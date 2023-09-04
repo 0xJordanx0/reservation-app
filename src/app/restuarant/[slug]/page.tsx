f45acf18065ca8fa680303feadfbe02dfdbd829e
0xJordanx0 <ahmedjamlan12@gmail.com>
2023-09-04 23:41:22 +0400

    Implemented a Restuarant main page

--- src/app/restuarant/[slug]/page.tsx
+++ src/app/restuarant/[slug]/page.tsx
@@ -1,5 +1,19 @@
+import Header from "./components/Header";
+import Overview from "./components/Overview";
+import Reservation from "./components/Reservation";
+import RestuarantNavbar from "./components/RestaurantNavbar";
 export default function Resturant() {
   return (
-    <div>Resturant</div>
-  )
+    <section>
+      <Header />
+      <div className="-mt-20 max-w-7xl mx-auto p-6 grid gap-[5%] grid-cols-[67.5%,27.5%]">
+        <div className="bg-white rounded-lg border-[1px] p-4">
+          <RestuarantNavbar />
+          <hr className="my-4" />
+          <Overview />
+        </div>
+        <Reservation />
+      </div>
+    </section>
+  );
 }
