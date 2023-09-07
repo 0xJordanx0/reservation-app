d78487e29163d0300fe0490eee992b3cbdfb1154
0xJordanx0 <ahmedjamlan12@gmail.com>
2023-09-07 23:44:00 +0400

    Implemented Fetching All Restauarants

--- src/app/components/ResturantItem.tsx
+++ src/app/components/ResturantItem.tsx
@@ -1,23 +1,26 @@
 import Link from "next/link";
+import { Restuarant } from "../../../types/global";
 
-export default function ResturantItem() {
+interface Props {
+  restaurant: Restuarant;
+}
+export default function ResturantItem({ restaurant }: Props) {
   return (
     <div className="max-w-sm bg-white border border-orange border-gray-200 rounded-lg shadow-md">
-      <a href="#">
-        <img className="rounded-t-lg" src="/resturant.webp" />
-      </a>
+      <Link href={`/restuarant/${restaurant.slug}`}>
+        <img className="rounded-t-lg" src={restaurant.main_image} />
+      </Link>
       <div className="p-5">
         <a href="#">
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
-            Alfonso Restuarant
+            {restaurant.name}
           </h5>
         </a>
         <p className="mb-3 font-normal text-gray-700">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa magnam
-          animi magni eum deleniti laudantium!
+          {restaurant.description}
         </p>
         <Link
-          href="#"
+          href={`/restuarant/${restaurant.slug}`}
           className="inline-flex items-center w-full justify-center px-3 py-2 text-sm font-medium text-center text-white bg-orange-700 rounded-lg hover:bg-orange-800 focus:ring-0 focus:outline-none"
         >
           View Food
--- src/app/components/ResturantListing.tsx
+++ src/app/components/ResturantListing.tsx
@@ -1,10 +1,32 @@
+import { Restuarant } from "../../../types/global";
 import ResturantItem from "./ResturantItem";
-export default function ResturantListing() {
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+const fetchRestauarants = async (): Promise<Restuarant[]> => {
+  const restaurants = await prisma.restaurant.findMany({
+    select: {
+      id: true,
+      name: true,
+      description: true,
+      main_image: true,
+      cuisine: true,
+      location: true,
+      price: true,
+      slug: true,
+    },
+  });
+
+  return restaurants;
+}
+
+export default async function ResturantListing() {
+  const restaurants = await fetchRestauarants();
   return (
     <section className="max-w-7xl mx-auto p-12">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 justify-items-center">
-        {Array.from({length:3}, () => (
-            <ResturantItem />
+        {restaurants.map((restaurant) => (
+          <ResturantItem restaurant={restaurant} />
         ))}
       </div>
     </section>
