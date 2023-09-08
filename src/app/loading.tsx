e6c04043e931b18c1ea48c3d0ef4f4e037c41361
0xJordanx0 <ahmedjamlan12@gmail.com>
2023-09-08 21:19:31 +0400

    Created loader components

---
+++ src/app/loading.tsx
@@ -0,0 +1,10 @@
+
+export default function Loading() {
+  return (
+    <main>
+      <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
+       Loading
+      </div>
+    </main>
+  );
+}
