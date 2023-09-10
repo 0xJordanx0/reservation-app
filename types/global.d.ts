cdc8c2133c73af0553b1ea9b3503d163258d6016
0xJordanx0 <ahmedjamlan12@gmail.com>
2023-09-10 21:48:37 +0400

    Added User Type

--- types/global.d.ts
+++ types/global.d.ts
@@ -1,36 +1,45 @@
 import { Cuisine, Location, PRICE } from "@prisma/client";
 
+// types
+
+type User = {
+  username: string;
+  password: string;
+  first_name: string;
+  last_name: string;
+};
+
 type SortOption = {
-    name: string,
-    href: string,
-    current: boolean
-}
+  name: string;
+  href: string;
+  current: boolean;
+};
 
 //Interfaces
 
-interface Restuarant{
-    id: number,
-    name: string,
-    description: string,
-    main_image: string,
-    cuisine: Cuisine,
-    location: Location,
-    price: PRICE,
-    slug: string
+interface Restuarant {
+  id: number;
+  name: string;
+  description: string;
+  main_image: string;
+  cuisine: Cuisine;
+  location: Location;
+  price: PRICE;
+  slug: string;
 }
 
 interface RestuarantInfo {
-    id: number,
-    name: string,
-    description: string,
-    images: string[],
-    slug: string
+  id: number;
+  name: string;
+  description: string;
+  images: string[];
+  slug: string;
 }
 
 interface SearchParams {
-    city?: string, 
-    cuisine?: string, 
-    price?: PRICE 
+  city?: string;
+  cuisine?: string;
+  price?: PRICE;
 }
 
-export {SortOption, Restuarant, RestuarantInfo, SearchParams};
+export { SortOption, User, Restuarant, RestuarantInfo, SearchParams };
